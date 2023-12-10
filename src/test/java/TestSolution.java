4a5e7b3f37804fcad9573ac7fa9145760e93a080
2021112514 <126329715+2021112514@users.noreply.github.com>
2023-12-10 21:37:13 +0800

    Update TestSolution.java
    Wrong test

--- src/test/java/TestSolution.java
+++ src/test/java/TestSolution.java
@@ -8,7 +8,7 @@ public class TestSolution {
         Solution solution = new Solution();
         assertEquals(0,solution.compareVersion("1.01","1.001"));
         assertEquals(0,solution.compareVersion("1.0","1.0.0"));
-        assertEquals(-1,solution.compareVersion("0.1","1.1"));
+        assertEquals(1,solution.compareVersion("0.1","1.1"));
         assertEquals(-1,solution.compareVersion("1.0.0.1","1.0.1"));
         assertEquals(1,solution.compareVersion("1.1","0.1"));
         assertEquals(1,solution.compareVersion("1.0.1","1.0.0.1"));
